8191d0c6a5c92c2c37dd2f1961784f98b83b581f
kazu <kazu0723_44@icloud.com>
2019-06-27 17:14:36 +0900

    new file:   CTS/.editorconfig 	new file:   CTS/.eslintrc.js 	new file:   CTS/.gitignore 	new file:   CTS/README.md 	new file:   CTS/assets/README.md 	new file:   CTS/components/AppLogo.vue 	new file:   CTS/components/README.md 	new file:   CTS/layouts/README.md 	new file:   CTS/layouts/default.vue 	new file:   CTS/middleware/README.md 	new file:   CTS/nuxt.config.js 	new file:   CTS/package-lock.json 	new file:   CTS/package.json 	new file:   CTS/pages/README.md 	new file:   CTS/pages/cookie.vue 	new file:   CTS/pages/index.vue 	new file:   CTS/plugins/README.md 	new file:   CTS/static/README.md 	new file:   CTS/static/favicon.ico 	new file:   CTS/store/README.md 	new file:   CTS/store/actions.js 	new file:   CTS/store/index.js 	new file:   CTS/store/mutations.js 	new file:   CTS/store/state.js

---
+++ CTS/pages/cookie.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+  created() {
+    const setCached =
+    {
+      title: 'こんにちは！タイトルをキャッシュします',
+      content: 'こんにちは！内容をキャッシュします。'
+    }
+    this.$cookies.set('article01', setCached, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7
+    })
+  }
+}
+</script>
---
+++ CTS/pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <article v-if = "article">
+      <h1>{{article.title}}</h1>
+      <p>{{article.content}}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    const setCached =
+    {
+      title: 'こんにちは！タイトルをキャッシュします',
+      content: 'こんにちは！内容をキャッシュします。'
+    }
+    this.$cookies.set('article01', setCached, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7
+    })
+  },
+  data () {
+    return {
+      article01: this.$cookies.get('article01')
+    }
+  },
+  computed: {
+    article() {
+      let post
+      if (this.article01) {
+        post = JSON.parse(this.article01)
+      }
+      return post
+    }
+  }
+}
+</script>
+
+<style>
+</style>
